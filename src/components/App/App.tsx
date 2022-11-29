283d5546d3728fd4476854bc6817d3900fa004e7
olvneto <olvneto@yahoo.com.br>
2022-11-29 10:36:13 -0300

    Aulas 2.9

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from '../Header';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
+import Input from '../../shared/Input';
+
+function TestComponent () {
+  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
+}
 
 function App() {
+  const [street, setStreet] = useState('')
   return (
     <div className="App">
       <Header title="AlgaStock"/>
-      App
+      <Container>
+        <Button content="Botão" onClick={() => window.alert('Aqui')} appendIcon={<TestComponent />}>
+          Alert
+        </Button>
+        <Input
+          label="Street"
+          placeholder="E.g.: 15h Avenue"
+          value={street}
+          onChange={e => setStreet(e.target.value)}
+        />
+      </Container>
     </div>
   );
 }
