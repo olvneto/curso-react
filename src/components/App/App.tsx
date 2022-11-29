ca80697ffc9a54c5b9bd8ae352f20cb460b11b72
olvneto <olvneto@yahoo.com.br>
2022-11-29 18:03:22 -0300

    Aula até 2.12

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,14 @@ import React, {useState} from 'react';
 import './App.css';
 import Container from '../../shared/Container';
 import Header from './../Header/Header';
+import Table from './../../shared/Table/Table';
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock"/>
       <Container>
-        
+        <Table />
       </Container>
     </div>
   );
--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import './Table.scss';
+//import './Table.scss';
 
 const Table = ()=> {
     return <table>
-        
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Cookie</td>
+            <td>$1.20</td>
+            <td>45</td>
+          </tr>            
+        </tbody>
     </table>
 }
 
