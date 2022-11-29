888facb2bf120f7bfbe22caa0f935a84288606fb
olvneto <olvneto@yahoo.com.br>
2022-11-28 21:12:02 -0300

    Aulas 2.3 e 2.4

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
 class ClassComponent extends React.Component {
-    state ={
+  constructor(props: any){
+    super(props)
+    console.log('Constructing ClassComponent')
+  }  
+  
+  state ={
         name: 'Mundo'
+    }
+
+    componentDidMount() {
+      console.log('Mounting atingido')
+    }
+
+    componentDidUpdate(){
+      console.log('Update atingido')
     }
 
 
     render(){
+      console.log('Render')
         return <div>
           <p>name: {this.state.name}</p>
           <button onClick={() => {
